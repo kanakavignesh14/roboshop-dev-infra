926d2a524ccae477d1f1df31101326266b0c6613
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-11-09 20:01:17 +0530

    m

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,6 +2,6 @@
 
 dnf install ansible -y
 ansible-pull -U https://github.com/kanakavignesh14/ansible-roboshop-roles.tf.git -e component=$component main.yaml
- git clone ansible-playbook
- cd ansible-playbook
- ansible-playbook -i inventory main.yaml
+ #git clone ansible-playbook
+ #cd ansible-playbook
+ #ansible-playbook -i inventory main.yaml
