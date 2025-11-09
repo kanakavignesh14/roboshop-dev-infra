537cc68e22a50ba4e6b7995aa5a11684a20def05
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-11-09 22:43:52 +0530

    ff

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -35,8 +35,8 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh",
-        #"sudo sh /tmp/bootstrap.sh mongodb"
+        #"sudo sh /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mongodb"
     ]
   }
 
