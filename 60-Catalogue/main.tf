7224169bbbf618d5860057d3550831ab5e18cc49
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-11-14 12:16:34 +0530

    kk

--- 60-Catalogue/main.tf
+++ 60-Catalogue/main.tf
@@ -4,9 +4,9 @@ resource "aws_instance" "catalogue_ec2" {
   subnet_id = local.private_subnet_ids # we are creating mongodb in database subnet # reffering local.tf
   vpc_security_group_ids = [data.aws_ssm_parameter.sg_id.value]  #reffering data.tf
   tags = {
-    Name = "mongodb-ec2"
+    Name = "catalogue-ec2"
     Environment = "dev"
-    ec2 = "mongodb_ec2"
+    ec2 = "catalogue_ec2"
 
 
   }
@@ -24,7 +24,8 @@ resource "terraform_data" "catalogue" {
     host     = aws_instance.catalogue_ec2.private_ip
   }
 
-  # terraform copies this file to mongodb server
+  # terraform copies this file to catalogue server
+
   provisioner "file" {
     source = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
---
+++ 60-Catalogue/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z04092822ZCUHU7SWZ18H"
+}
+
+variable "domain_name" {
+    default = "vigi-devops.fun"
+}
