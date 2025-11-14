9894655fc8f4276e35c457b15c994a5cce211dc1
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-11-14 12:25:49 +0530

    kk

--- 60-Catalogue/local.tf
+++ 60-Catalogue/local.tf
@@ -1,5 +1,5 @@
 locals{
     #subnet_id = data.aws_ssm_parameter.
-    private_subnet_ids = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
+    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     ami_id = data.aws_ami.joindevops.id
 }
