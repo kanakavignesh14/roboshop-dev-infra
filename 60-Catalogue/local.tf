245a45d4c0bc35842e0074322451ba479ea64a68
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-11-15 10:49:31 +0530

    dd

--- 60-Catalogue/local.tf
+++ 60-Catalogue/local.tf
@@ -1,6 +1,7 @@
 locals{
     #subnet_id = data.aws_ssm_parameter.
-    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
+    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    #private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     ami_id = data.aws_ami.joindevops.id
     common_name_suffix ="${var.project_name}-${var.environment}" 
 }
