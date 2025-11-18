43f1948770499d3b3156718abc7ce8bc46c0e2f4
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-11-18 12:51:15 +0530

    cc

--- 90-component-test/main.tf
+++ 90-component-test/main.tf
@@ -1,5 +1,5 @@
 module "component" {
-    source = "../terraform-roboshop-componet"
+    source = "../../terraform-roboshop-componet"
     component_name = var.component_name
     ami_id = data.aws_ami.joindevops.id
     rule_priority = var.rule_priority
