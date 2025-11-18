755b0d4a4e347e1ab8831a73fd6d73da5792f5fb
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-11-18 16:34:15 +0530

    cc

--- 90-component-test/main.tf
+++ 90-component-test/main.tf
@@ -1,8 +1,17 @@
-module "component" {
-    source = "../../terraform-roboshop-componet"
-    component_name = var.component_name
-    ami_id = data.aws_ami.joindevops.id
-    rule_priority = var.rule_priority
+#module "component" {
+ #   source = "../../terraform-roboshop-componet"
+  #  component_name = var.component_name
+ #   ami_id = data.aws_ami.joindevops.id
+  #  rule_priority = var.rule_priority
 
 
+#}
+
+
+
+module "components" {
+    for_each = var.components
+    source = "git::https://github.com/kanakavignesh14/terraform-roboshop-componet.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
 }
